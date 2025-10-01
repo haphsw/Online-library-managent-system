f0c16cba8c7196d9c622ea6daa7ea6851e9bc525
haphsw <hafswhussein@gmail.com>
2025-10-01 16:00:53 +0300

    Update Book.cpp

--- src/Book.cpp
+++ src/Book.cpp
@@ -1,1 +1,12 @@
+#include "Book.h"
+
+Book::Book(string t, string a, string i, int year, int copies, int available){
+  title = t;
+  author = a;
+  isbn = i;
+  publishedYear = year;
+  totalCopies = copies;
+  availableCopies = available;
+  
+}
 
