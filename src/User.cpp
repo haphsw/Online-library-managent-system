dbdad712b6b1141e70b1fc23af66ceedd185a9cd
haphsw <hafswhussein@gmail.com>
2025-10-01 17:36:12 +0300

    Update User.cpp

--- src/User.cpp
+++ src/User.cpp
@@ -1,1 +1,31 @@
+#include "User.h"
 
+User::User(int i, string n) {
+    id = i;
+    name = n;
+}
+
+int User::getId() {
+    return id;
+}
+
+string User::getName() {
+    return name;
+}
+
+void User::borrowBook(string isbn) {
+    borrowedBooks.push_back(isbn);
+}
+
+void User::returnBook(string isbn) {
+    for (int k = 0; k < borrowedBooks.size(); k++) {
+        if (borrowedBooks[k] == isbn) {
+            borrowedBooks.erase(borrowedBooks.begin() + k);
+            break;
+        }
+    }
+}
+
+vector<string> User::getBorrowedBooks() {
+    return borrowedBooks;
+}
