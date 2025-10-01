7cc81922a68007e20ec6e3ba6d35e52e4920048f
haphsw <hafswhussein@gmail.com>
2025-10-01 16:38:07 +0300

    Update Book.h

--- src/Book.h
+++ src/Book.h
@@ -2,7 +2,7 @@
 using namespace std;
 
 class Book{
-public:
+private:
   string title;
   string author;
   string isbn;
@@ -10,7 +10,23 @@ public:
   int totalCopies;
   int availableCopies;
 
+public:
   Book(string t, string a, string i, int year, int copies, int available);
+
+  string getTitle();
+  string getAuthor();
+  string getISBN();
+  int getPublishedYear();
+  int getTotalCopies();
+  int getAvailableCopies();
+
+  void setTitle(string t);
+  void setAuthor(string a);
+  void setISBN(string i);
+  void setPublishedYear(int year);
+  void setTotalCopies(int copies);
+  void setAvailableCopies(int available);
+
 
   bool borrowBook();
   bool returnBook();
