15bc222272281c4b2908958ca1cffaffc45247fe
haphsw <hafswhussein@gmail.com>
2025-10-01 15:46:13 +0300

    Update Book.h

--- src/Book.h
+++ src/Book.h
@@ -1,1 +1,20 @@
-// Class: Book
+#include <string>
+using namespace std;
+
+class Book{
+public:
+  string title;
+  string author;
+  string isbn;
+  int publishedYear;
+  int totalCopies;
+  int availableCopies;
+
+  Book(string t, string a, string i, int year, int copies);
+
+  bool borrowBook();
+  bool returnBook();
+      
+};
+
+
