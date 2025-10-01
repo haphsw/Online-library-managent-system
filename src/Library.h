f0a8bac86df10c9eefc0de5e116a1328ab5c4d8c
haphsw <hafswhussein@gmail.com>
2025-10-01 19:19:39 +0300

    Update Library.h

--- src/Library.h
+++ src/Library.h
@@ -1,1 +1,24 @@
 
+#include <string>
+#include "Book.h"
+#include "User.h"
+using namespace std;
+
+class Library {
+private:
+    vector<Book> books; 
+    vector<User> users; 
+
+public:
+    void addBook(Book book);
+    void removeBook(string bookTitle);
+    int searchBookByTitle(string title); 
+    vector<int> searchBookByAuthor(string author); 
+
+    void addUser(User user);
+    void removeUser(int userId);
+    int getUserIndex(int userId); 
+
+    void borrowBook(int userId, string bookTitle);
+    void returnBook(int userId, string bookTitle);
+};
