cea59cfac4b5dc0006d43bba6c59034c458baa4d
haphsw <hafswhussein@gmail.com>
2025-10-01 17:06:49 +0300

    Update User.h

--- src/User.h
+++ src/User.h
@@ -1,1 +1,25 @@
+#include <string>
+#include <vector>
+using namespace std;
 
+class User {
+private:
+    int id;                      
+    string name;                   
+    vector<string> borrowedIsbns; 
+
+public:
+    User(int id, string name);
+
+   // Getters
+    int getId();
+    string getName();
+    vector<string> getBorrowedIsbns();
+
+    // Setters
+    void setName(string n);
+  
+    bool hasBorrowed(const string &isbn);      
+    void borrowBook(const string &isbn);       
+    void returnBook(const string &isbn);
+};
