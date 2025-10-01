c13c590ebbacd1ad5bb2ef4370028064eb1d1ebf
haphsw <hafswhussein@gmail.com>
2025-10-01 16:10:15 +0300

    Update Book.cpp

--- src/Book.cpp
+++ src/Book.cpp
@@ -9,4 +9,20 @@ Book::Book(string t, string a, string i, int year, int copies, int available){
   availableCopies = available;
   
 }
+bool Book::borrowBook(){
+  if (availableCopies > 0){
+    availableCopies--;
+    return true;
+  }
+  return false;
+  
+}
 
+bool Book::returnBook(){
+  if (availableCopies < totalCopies){
+    availableCopies++;
+    return true;
+  }
+  return false;
+  
+}
